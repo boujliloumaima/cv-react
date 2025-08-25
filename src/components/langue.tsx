c981031609853f6dc0cc0191b3543eeacd2f7e23
walidnoussir <boujlilomaima@gmail.com>
2025-08-25 13:05:35 +0100

    feat: apdate langue component

--- src/components/langue.tsx
+++ src/components/langue.tsx
@@ -1,40 +1,15 @@
-import { Langues } from "../models";
-export default function MyLangues() {
-  const myLangues: Langues = {
-    langues: [
-      {
-        nom: "Arabe",
-        niveau: "Langue Maternelle",
-      },
-      {
-        nom: "Françans",
-        niveau: "Lu, écrit, parlé",
-      },
-      {
-        nom: "Arabe",
-        niveau: "technique",
-      },
-    ],
-  };
+import { Lang } from "../models";
+
+interface Props {
+  lang: Lang;
+}
+
+export default function UserLanguage({ lang }: Props) {
   return (
     <div>
-      {" "}
-      <h3>Langues</h3>
-      <div className="exp">
-        {myLangues.langues.map((el) => {
-          return (
-            <>
-              {" "}
-              <div>
-                <p>{el.nom}</p>
-              </div>
-              <div className="test">
-                <p>{el.niveau}</p>
-              </div>
-            </>
-          );
-        })}
-      </div>
+      <p>
+        {lang.name} - {lang.level}
+      </p>
     </div>
   );
 }
