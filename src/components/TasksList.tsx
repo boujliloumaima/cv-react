208a29673b0b91d1d4cc361cf5beaea79a5663a6
walidnoussir <boujlilomaima@gmail.com>
2025-08-25 13:04:06 +0100

    feat: add TasksList component

---
+++ src/components/TasksList.tsx
@@ -0,0 +1,16 @@
+interface Props {
+  tasks: string[];
+}
+
+export default function TasksList({ tasks }: Props) {
+  return (
+    <div>
+      <strong>Tasks:</strong>
+      <ul>
+        {tasks.map((task, idx) => (
+          <li key={idx}>{task}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
